f7f87907c9144ed72f01ffb6bb780b79621e0b61
calumtreloar <calumtreloar@gmail.com>
2023-06-28 18:25:08 +0930

    Add hover effect for squares

--- script.js
+++ script.js
@@ -5,4 +5,33 @@ for (let i = 0; i < gridSize; i++) {
   const gridSquare = document.createElement("div");
   gridSquare.className = "grid-square";
   container.appendChild(gridSquare);
+  changeColour();
 }
+
+function changeColour() {
+  let squares = document.querySelectorAll("div.grid-square");
+  squares.forEach((element) => {
+    element.addEventListener("mouseover", () => {
+      element.style.backgroundColor = "red";
+    });
+    element.addEventListener("mouseout", () => {
+      element.style.backgroundColor = "";
+    });
+  });
+}
+
+function mouseOver() {
+  document.getElementsByClassName("grid-square").style.color = "red";
+}
+
+function mouseOut() {
+  document.getElementsByClassName("grid-square").style.color = "";
+}
+
+// Add a hover effect to the squares using event listeners in the for loop
+// Change the div's background colour while mouse is hovering
+
+// Add a button to top of screen
+// Ask for user to enter a number that will create a nxn grid
+// If number > 100 or < 0, ask for another number
+// Use functions where appropriate to clean code up
